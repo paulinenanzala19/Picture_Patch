4d8f9a233d51b40deae646736fe11283fa7c7d6f
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-27 12:06:48 +0300

    include the pictures url

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,12 @@
+from django.http  import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    title='Zoomin'
+    images=Images.object.all()
+    location=Location.object.all()
+    categories=category.object.all()
+
+    return render(request,'index.html',{'images':images,'location':location,'categories':categories,'title':title})
+
