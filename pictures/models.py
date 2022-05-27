9f8593a81297eb4318e51697a38c728c0f1c0c76
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-27 15:09:47 +0300

    adding bootstrap

--- pictures/models.py
+++ pictures/models.py
@@ -5,7 +5,7 @@ class Image(models.Model):
     image=models.ImageField(upload_to = 'images/', unique=True, null=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    date = models.DateTimeField(auto_now_add=False,auto_now=True)
+    date = models.DateTimeField(auto_now_add=True,auto_now=False)
     category = models.ForeignKey('Category',on_delete=models.CASCADE) 
     location = models.ForeignKey('Location',on_delete=models.CASCADE,default=1) 
 
