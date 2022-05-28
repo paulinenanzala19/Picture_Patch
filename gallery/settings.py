7f66f06d6aa6fc74b25bc25d6c2f6b8b135a8979
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-28 09:57:35 +0300

    registering the media root route

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Application definition
 
@@ -69,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
--- pictures/urls.py
+++ pictures/urls.py
@@ -6,3 +6,5 @@ urlpatterns=[
     path('',views.home, name='landing'),
     path('search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
